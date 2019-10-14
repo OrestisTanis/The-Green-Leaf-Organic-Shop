c23012e352d19bda30e32c1df024c7419a2f8354
Orestis Tanis <tanisorestis@yahoo.com>
2019-10-14 19:29:10 +0300

    Setup and Navbar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCtK7dvB8X11KSupX6r78HxjnExkX4iwhE",
+    authDomain: "oshop-bb9c1.firebaseapp.com",
+    databaseURL: "https://oshop-bb9c1.firebaseio.com",
+    projectId: "oshop-bb9c1",
+    storageBucket: "oshop-bb9c1.appspot.com",
+    messagingSenderId: "562508097386",
+    appId: "1:562508097386:web:576f09cbf6b20e7dc46713",
+    measurementId: "G-B2HRLLB22C"
+  }
 };
 
 /*
