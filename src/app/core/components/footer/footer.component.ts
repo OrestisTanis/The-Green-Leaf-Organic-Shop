c344e8c8079f036b82f97a123bc7d48265fad07f
Orestis Tanis <tanisorestis@yahoo.com>
2019-11-15 12:37:01 +0200

    New Footer Part I

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  // Font-Awesome Icons
+  
   constructor() { }
 }
